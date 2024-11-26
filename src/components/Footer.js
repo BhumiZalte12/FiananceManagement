49d71972845533d0fcbd9872930154db1cada5fb
Bhumi Sahebrao Zalte <111906536+BhumiZalte12@users.noreply.github.com>
2024-11-26 19:52:57 +0530

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+	return (
+		<footer>
+			<small>&copy; developed by</small>
+			
+				<img src="/logo.png" alt="logo" title="developed by FinBuddy" />
+			
+		</footer>
+	);
+}
